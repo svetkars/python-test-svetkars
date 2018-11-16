24cd9bb1e9acd3ef98f1b0f0324069faa5f52672
Svetlana <svetlana.karslioglu@rackspace.com>
2018-11-16 13:02:31 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='hr',
+    version='0.1.0',
+    description='CMD user management utility',
+    long_description=readme,
+    author='Svetlana Karslioglu',
+    author_email='svetlana.karslioglu@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
